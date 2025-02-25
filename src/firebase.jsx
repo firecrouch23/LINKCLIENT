5215197d27fd3d3260a240be749d0321b2088636
firecrouch23 <firecrouch23@gmail.com>
2025-02-26 03:13:21 +0700

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5i-9GjClVv55yNrNRTlsopklXnbeFZvM",
-  authDomain: "linkproject-687fa.firebaseapp.com",
-  projectId: "linkproject-687fa",
-  storageBucket: "linkproject-687fa.firebasestorage.app",
-  messagingSenderId: "562283216290",
-  appId: "1:562283216290:web:eb31bdfcf920cfa97ac6fb"
+  apiKey: "AIzaSyBitFVYdKrGlYkd7mxwfV6IiraTsfsivwQ",
+  authDomain: "linkproject-2e04a.firebaseapp.com",
+  projectId: "linkproject-2e04a",
+  storageBucket: "linkproject-2e04a.firebasestorage.app",
+  messagingSenderId: "732289081112",
+  appId: "1:732289081112:web:eecd4c5d4333637af8c86d"
 };
 
 const app = initializeApp(firebaseConfig);
