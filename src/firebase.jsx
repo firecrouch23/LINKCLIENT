3970b9ee6eb96c4af92c5a74c68d8a349c69bfea
firecrouch23 <firecrouch23@gmail.com>
2025-03-08 21:35:52 +0700

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAgvZw0DZ931kUtaGyX7jRjTcbkASOwUEg",
-  authDomain: "link03-25908.firebaseapp.com",
-  projectId: "link03-25908",
-  storageBucket: "link03-25908.firebasestorage.app",
-  messagingSenderId: "561452455527",
-  appId: "1:561452455527:web:e32eb40d0b257e1e7866c2"
+onst firebaseConfig = {
+  apiKey: "AIzaSyBjPHGuybkaa7e6CaUUqhUGNtBpvShltWs",
+  authDomain: "link03-cd187.firebaseapp.com",
+  projectId: "link03-cd187",
+  storageBucket: "link03-cd187.firebasestorage.app",
+  messagingSenderId: "111150283663",
+  appId: "1:111150283663:web:05a5870fa8d695375b69fa"
 };
 
 
